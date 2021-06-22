509b58f86f6b4ad87581cf8d52c14eaac352d41b
AmeerYHassan <ayh7@njit.edu>
2021-06-22 17:53:37 -0400

    added slash commands

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import commands
+from discord_slash import SlashCommand
 import time
 import os
 import json
@@ -8,6 +10,10 @@ client = discord.Client()
 BOT_PREFIX = '_$'
 logged_channels = {}
 message_dict = {}
+
+slash = SlashCommand(client, sync_commands=True)
+voice_channel_id = None
+text_channel_id = None
 
 with open('notifications.json') as json_file:
     message_dict = json.load(json_file)
@@ -21,6 +27,19 @@ def generate_message(message_type, member_name):
     return f"{message_dict[message_type]['emoji']} {(message_dict[message_type]['message']).replace('{member}', '**'+str(member_name)+'**')}"
 
 # Bot Functions #
+@slash.slash(description="Set the voice channel to monitor and the text channel to send messages to")
+async def set_voice(ctx, voice_channel_name, text_channel_name):
+    global voice_channel_id, text_channel_id
+    try:
+        voice_channel = discord.utils.get(ctx.guild.channels, name=voice_channel_name)
+        voice_channel_id = voice_channel.id
+        await ctx.send(f"Voice chat tracking has been set to the **{voice_channel}** voice chat")
+        
+        text_channel = discord.utils.get(ctx.guild.channels, name=text_channel_name)
+        text_channel_id = text_channel.id
+        await ctx.send(f"The text channel has been set to the **{text_channel}** text chat")
+    except:
+        await ctx.send(f"{voice_channel_name} channel was not found.")
 
 @client.event
 async def on_ready():
@@ -35,10 +54,13 @@ async def on_message(message):
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    global logged_channels
+    global logged_channels, voice_channel_id, text_channel_id
 
-    voice_channel_text = client.get_channel(856020467995377674) # Fix later on, find out how to make this set by the user.
-    voice_channel = client.get_channel(856016214597566479)
+    if (not voice_channel_id or not text_channel_id):
+        return
+
+    voice_channel_text = client.get_channel(text_channel_id) # Fix later on, find out how to make this set by the user.
+    voice_channel = client.get_channel(voice_channel_id)
 
     curr_embed = discord.Embed(
         title=f"Update to the {after.channel} channel",
@@ -56,47 +78,47 @@ async def on_voice_state_update(member, before, after):
         curr_embed.description = generate_message('user_joined', member)
         curr_embed.colour = discord.Colour.green()
         logged_channels[after.channel.id]['user_list'][str(member)] = time.time()
-        logged_channels[after.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+        logged_channels[after.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
     # User Left
     elif (before.channel != None and after.channel == None):
         elapsed_epoch = time.gmtime(time.time()-logged_channels[before.channel.id]['user_list'][str(member)])
         elapsed_time = time.strftime('%H:%M:%S', elapsed_epoch)
         curr_embed.description = generate_message('user_left', member)
         curr_embed.colour = discord.Colour.red()
-        logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+        logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}, `{elapsed_time}`")
     else:
         # unmuted -> self muted
         if (not before.self_mute and after.self_mute):
             curr_embed.description = generate_message('user_muted', member)
             curr_embed.colour = discord.Colour.red()
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
         # self muted -> unmuted
         if (before.self_mute and not after.self_mute):
             curr_embed.description = generate_message('user_unmuted', member)
             curr_embed.colour = discord.Colour.red()
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
         # undeafened -> self deafened
         if (not before.self_deaf and after.self_deaf):
             curr_embed.description = generate_message('user_deafened', member)
             curr_embed.colour = discord.Colour.red()    
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
         # self deafened -> undeafened
         if (before.self_deaf and not after.self_deaf):
             curr_embed.description = generate_message('user_undeafened', member)
             curr_embed.colour = discord.Colour.red()    
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
         # not streaming -> streaming
         if (not before.self_stream and after.self_stream):
             curr_embed.description = generate_message('user_started_streaming', member)
             curr_embed.colour = discord.Colour.red()
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")
         # streaming -> not streaming
         if (before.self_stream and not after.self_stream):
             curr_embed.description = generate_message('user_stopped_streaming', member)
             curr_embed.colour = discord.Colour.red()
-            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` - {curr_embed.description}")            
+            logged_channels[before.channel.id]['events_steps'].append(f"`{get_curr_time()}` ├ {curr_embed.description}")            
 
-    await voice_channel_text.send(embed=curr_embed)
+    # await voice_channel_text.send(embed=curr_embed)
 
     # If no one is in the voice channel... 
     if (len(voice_channel.members) == 0):
